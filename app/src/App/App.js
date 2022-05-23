8a2adc9dfd0495274e4a9e00b9311b95bd224c91
AlixLeBert <alix.le_bert@fioulmarket.fr>
2022-05-23 15:31:31 +0200

    props style

--- app/src/App/App.js
+++ app/src/App/App.js
@@ -5,8 +5,16 @@ import Button from './components/Button/Button';
 function App() {
   return (
     <div className="App">
-      <Button text="hello"/>
-      <Button text="click"/>
+      <Button text="hello"
+        onButtonClicked={() => {
+          console.log('toto');
+        }}
+        bgcolor="tomato"
+        color="brown"
+      />
+
+      <Button text="click" styleNew={{textDecoration:'underline blue', fontStyle:'italic'}}/>
+      <Button text="button3" styleNew={{backgroundColor:'grey'}} bgcolor="blue"/>
     </div>
   );
 }
--- app/src/App/components/Button/Button.js
+++ app/src/App/components/Button/Button.js
@@ -1,11 +1,40 @@
 import React from "react";
-import { isPropertyAccessOrQualifiedName } from "typescript";
+import style from './Button.module.css'
+import PropTypes from 'prop-types';
 
 
 // contenu du component
 function Button(props) {
-    console.warn(props);
-    return <button>{props.text}</button>;
+    return (
+    <button className={style.Button}
+        onClick={(evt) => {
+            if (undefined !== props.onButtonClicked && typeof props.onButtonClicked === 'function')
+            props.onButtonClicked()
+        }}
+        style={{
+            ...props.styleNew,
+            backgroundColor:props.bgcolor,
+            color:props.color
+        }}
+        type={props.type}
+    >
+        {props.text}
+    </button>
+    );
+}
+
+Button.propTypes={
+  bgcolor: PropTypes.string.isRequired, 
+  styleNew: PropTypes.object,
+  color: PropTypes.any.isRequired,
+  onButtonClicked: PropTypes.func,
+  type: PropTypes.string,
+}
+
+Button.defaultProps={
+    bgcolor: 'skyblue',
+    color: 'white',
+    type: 'button',
 }
 
 // exportation
